bd060deec0e6f50fb2bfa55a772c7b73d0d20c4e
Marcos César de Oliveira <mcesarhm@gmail.com>
2017-03-03 11:31:49 -0300

    Allow match function for methods arguments

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var nock = require('nock')
 
 module.exports.mock = (c) => new Proxy({ uri: c.uri }, {
   get: (t, a) => (...args) => {
+    if (args.length !== 1) throw new Error('Only one argument is allowed')
+    let ea = typeof args[0] === 'object' ? client.encode(args) : args[0]
     if (client.isPostMethod(a)) {
-      return nock(t.uri).post('/' + client.postMethodName(a), client.encode(args))
+      return nock(t.uri).post('/' + client.postMethodName(a), ea)
     }
-    return nock(t.uri).get('/' + a).query(client.encode(args))
+    return nock(t.uri).get('/' + a).query(ea)
   }
 })
